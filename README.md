d6f4a6a36e4edfa54c24cb414304d9dc9ff8512f
Andrew Skurlatov <a.skurlatov@gmail.com>
2021-04-14 19:26:39 +0300

    feat: single batch for all ERC721

--- README.md
+++ README.md
@@ -5,8 +5,20 @@ It will allow to mint/transfer unlimited amounts of ERC721
 tokens to single address and it reduce GAS costs approximately by 10-30%.
 Contract should be deployed first to blockchain with address of related ERC721 contract
 
+## How it Works
+For first, you need to deploy Batch contract to Ethereum network.
 
-## Authors
+### Batch minting:
+ 1. Transfer Ownership to Batch contract from ERC721 contract by its calling ```transferOwnership``` method
+    1. For Communal ERC71 - call ``mintBatchCommunal`` method on Batch contract
+    2. For standard ERC71 - call ``mintBatch`` method on Batch contract
+ 2. You can revoke ERC721 contract Ownership by calling ``returnOwnership`` method on Batch contract
+
+### Batch transfer:
+ 1. Call ``setApprovalForAll`` method on ERC721 contract to allow token transfer to Batch contract
+ 2. Call ``transferBatch`` method on Batch contract
+
+## Author
 
 * **Andrey Skurlatov** - [andskur](tps://github.com/andskur)
 
--- contracts/Batch.sol
+++ contracts/Batch.sol
@@ -8,18 +8,15 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 * @title ERC721 contract interface
 *
 * @notice Interface of common ERC721 contracts
-* with allowance to mint communal NFT
+* with allowance to mint and transfer NFT
 * Using for batch minting and transfer
-*
-* @author Andskur
 */
-interface ERC721Communal {
+interface ERC721 {
   // @notice Function to mint new token to given address with token URI
   // @param to The address that will own the minted token
   // @param tokenURI string The token URI of the minted token
-  // @param communal - set token communal, true or false
   // @return uint256 is token id of new created token
-  function mint(address to, string tokenURI, bool communal) external returns (uint256);
+  function mint(address to, string calldata) external returns (uint256);
 
   // @notice Transfers the ownership of a given token ID to another address.
   // @param from current owner of the token
@@ -35,6 +32,21 @@ interface ERC721Communal {
   function owner() external view returns (address);
 }
 
+/**
+* @title ERC721 communal contract interface
+*
+* @notice Interface of communal ERC721 contracts
+* with allowance to mint communal NFT
+* Using for batch minting and transfer
+*/
+interface ERC721Communal {
+  // @notice Function to mint new communal token to given address with token URI
+  // @param to The address that will own the minted token
+  // @param tokenURI string The token URI of the minted token
+  // @param communal - set token communal, true or false
+  // @return uint256 is token id of new created token
+  function mint(address to, string calldata, bool communal) external returns (uint256);
+}
 
 // @title ERC721 Batch minting and transfer contract
 //
@@ -46,17 +58,6 @@ interface ERC721Communal {
 //
 // @author Andskur
 contract Batch is Ownable{
-  ERC721Communal public erc721;
-
-  /**
-  * @notice Batch contract constructor
-  * @dev Initializes the contract
-  * @param addr related ERC721 contact address
-  */
-  constructor(address addr) public {
-    erc721 = ERC721(addr);
-  }
-
   /**
   * @notice batch ERC721 minting
   * @dev mint given amount of NFT to given address
@@ -65,9 +66,23 @@ contract Batch is Ownable{
   * @param to The address that will own the minted tokens
   * @param tokens array of the tokens URI's of the minted tokens
   */
-  function mint_batch(address to, string[] memory tokens) public onlyOwner {
+  function mintBatch(address erc721, address to, string[] memory tokens) public onlyOwner {
     for(uint index=0; index<tokens.length; index++){
-      erc721.mint(to, tokens[index], true);
+      ERC721(erc721).mint(to, tokens[index]);
+    }
+  }
+
+  /**
+  * @notice batch communal ERC721 minting
+  * @dev mint given amount of communal NFT to given address
+  * it will be revert if caller not the Batch contract owner
+  * it will be revert if Batch contract no the owner of ERC721 contract
+  * @param to The address that will own the minted tokens
+  * @param tokens array of the tokens URI's of the minted tokens
+  */
+  function mintBatchCommunal(address erc721, address to, string[] memory tokens) public onlyOwner {
+    for(uint index=0; index<tokens.length; index++){
+      ERC721Communal(erc721).mint(to, tokens[index], true);
     }
   }
 
@@ -79,9 +94,9 @@ contract Batch is Ownable{
   * @param to The address that will own the transferred tokens
   * @param tokens array of the tokens ID's of the transferred tokens
   */
-  function transfer_batch(address to, uint256[] memory tokens) public {
+  function transferBatch(address erc721, address to, uint256[] memory tokens) public {
     for(uint index=0; index<tokens.length; index++){
-      erc721.transferFrom(message.sender, to, tokens[index]);
+      ERC721(erc721).transferFrom(msg.sender, to, tokens[index]);
     }
   }
 
@@ -90,7 +105,7 @@ contract Batch is Ownable{
   * @dev transfer ownership od ERC721 contract to given address
   * @param to address to returned ownership
   */
-  function returnOwnership(address to) public onlyOwner {
-    erc721.transferOwnership(to);
+  function returnOwnership(address erc721, address to) public onlyOwner {
+    ERC721(erc721).transferOwnership(to);
   }
 }
